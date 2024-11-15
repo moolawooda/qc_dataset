5ea0192ccfd335961d37f6e4f76102fd77340413
moolawooda <103896544+moolawooda@users.noreply.github.com>
2024-11-15 08:01:57 +0000

    bugfix
    wrong root in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="qcds",
     version="0.1.0",
     description="A dataset management tool for quantum computing",
-    packages=find_packages("qcds"),
+    packages=find_packages(),
     package_data={"qcds": ["templates/*"]},
     include_package_data=True,
 )
