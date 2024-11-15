2338773fa2059889da896620754235a76be3673d
moolawooda <103896544+moolawooda@users.noreply.github.com>
2024-11-15 06:27:09 +0000

    finish the dataset scan workflow

--- qcds/dataset/subset.py
+++ qcds/dataset/subset.py
@@ -32,9 +32,19 @@ class SubSet:
         self.mole_configs: list[MoleConfig] = []
         # self.mole_paths: dict = {}
         self.input_path: str = ""
+        self.out_path: str = ""
 
         self.dataset_eval: list[dict] = []
         self.mole_eng: dict = {}
+
+        self.abacus_pseudo_file_syntax: str = ""
+        self.params_abacus_input: dict = {}
+        self.params_abacus_stru: dict = {}
+        self.params_abacus_kpt: dict = {}
+        self.params_pyscf: dict = {}
+        self.params_gaussian: dict = {}
+        self.params_psi4: dict = {}
+        self.params_slurm: dict = {}
 
     @staticmethod
     def read_moleconfigs(
@@ -91,15 +101,91 @@ class SubSet:
 
         return eval_list
 
-    def input_gen_abacus(
-        self, params_input: dict, params_stru: dict, params_kpt: dict, params_sh: dict
+    def params_def_abacus(
+        self,
+        pseudo_dir: str,
+        pseudo_file_syntax: str,
+        calculation: str,
+        dft_functional: str,
+        ecutwfc: int,
+        lattice: float | None = None,
+        lattice_x: float | None = None,
+        lattice_y: float | None = None,
+        lattice_z: float | None = None,
     ):
-        def get_pot(element: str, pot_path: str) -> str:
-            # TODO
-            pot_file = f"PROJECT-{element}_HGH_NLCC.UPF-1.upf"
-            if not os.path.exists(os.path.join(pot_path, pot_file)):
-                raise FileNotFoundError(f"Potential file {pot_file} not found")
-            return pot_file
+        self.abacus_pseudo_file_syntax = pseudo_file_syntax
+
+        self.params_abacus_input = {
+            "pseudo_dir": pseudo_dir,
+            "calculation": calculation,
+            "dft_functional": dft_functional,
+            "ecutwfc": ecutwfc,
+        }
+
+        match (lattice, lattice_x, lattice_y, lattice_z):
+            case (None, None, None, None):
+                raise ValueError("Lattice parameters are missing")
+            case (None, _, _, _):
+                self.params_abacus_stru = {
+                    "LATTICE_X": lattice_x,
+                    "LATTICE_Y": lattice_y,
+                    "LATTICE_Z": lattice_z,
+                }
+            case (_, None, None, None):
+                self.params_abacus_stru = {
+                    "LATTICE_X": lattice,
+                    "LATTICE_Y": lattice,
+                    "LATTICE_Z": lattice,
+                }
+            case _:
+                raise ValueError("Lattice parameters are not consistent")
+
+    def params_def_pyscf(self, num_threads: int, basis: str, xc: str):
+        self.params_pyscf = {
+            "num_threads": num_threads,
+            "basis": basis,
+            "xc": xc,
+        }
+
+    def params_def_gaussian(self, mem: str, nproc: int, command: str):
+        self.params_gaussian = {
+            "mem": mem,
+            "nproc": nproc,
+            "command": command,
+        }
+
+    def params_def_psi4(self, num_threads: int, memory: str, basis: str, xc: str):
+        self.params_psi4 = {
+            "num_threads": num_threads,
+            "memory": memory,
+            "basis": basis,
+            "xc": xc,
+        }
+
+    def params_def_slurm(
+        self,
+        partition: str,
+        qos: str,
+        nodes: int,
+        task_per_node: int,
+        cores_per_task: int,
+        slurm_output: str = "job.%j.out",
+    ):
+        self.params_slurm = {
+            "partition": partition,
+            "qos": qos,
+            "nodes": nodes,
+            "task_per_node": task_per_node,
+            "cores_per_task": cores_per_task,
+            "slurm_output": slurm_output,
+        }
+
+    def input_gen_abacus(self):
+        def get_pp(element: str, pp_path: str, pp_syntax: str) -> str:
+            pp_file = pp_syntax.replace("{}", element)
+            if not os.path.exists(os.path.join(pp_path, pp_file)):
+                raise FileNotFoundError(f"Potential file {pp_file} not found")
+            return pp_file
 
         print("Generating input files for Abacus...")
 
@@ -120,6 +206,11 @@ class SubSet:
             if not os.path.exists(new_folder):
                 os.makedirs(new_folder)
 
+            params_input = self.params_abacus_input.copy()
+            params_stru = self.params_abacus_stru.copy()
+            params_kpt = self.params_abacus_kpt.copy()
+            params_slurm = self.params_slurm.copy()
+
             spin = mole.get_spin("pyscf")
             params_input.update(
                 {
@@ -139,8 +230,10 @@ class SubSet:
                         ]
                     )
                 }"
-                pot = get_pot(atom, params_input["pseudo_dir"])
-                potential_str += f"{atom} {elem_mass[atom]} {pot}\n"
+                pp = get_pp(
+                    atom, params_input["pseudo_dir"], self.abacus_pseudo_file_syntax
+                )
+                potential_str += f"{atom} {elem_mass[atom]} {pp}\n"
 
             params_stru.update(
                 {
@@ -149,12 +242,12 @@ class SubSet:
                 }
             )
 
-            params_sh.update(
+            params_slurm.update(
                 {
                     "jobname": mole.name,
                     "commands": (
                         "export OMP_NUM_THREADS=1\n"
-                        f"mpirun -np {params_sh["cores_per_task"]} abacus"
+                        f"mpirun -np {params_slurm["cores_per_task"]} abacus"
                     ),
                 }
             )
@@ -166,11 +259,11 @@ class SubSet:
             with open(os.path.join(new_folder, "KPT"), "w") as f:
                 f.write(kpt_template.substitute(params_kpt))
             with open(os.path.join(new_folder, f"{mole.name}.sh"), "w") as f:
-                f.write(sh_template.substitute(params_sh))
+                f.write(sh_template.substitute(params_slurm))
 
         print("Done!")
 
-    def input_gen_pyscf(self, params_pyscf: dict, params_sh: dict):
+    def input_gen_pyscf(self):
         print("Generating input files for PySCF...")
 
         input_file = pkg.resource_string("qcds", "templates/pyscf.template")
@@ -183,6 +276,9 @@ class SubSet:
             # self.mole_paths.update({mole.name: new_folder})
             print(f"> {mole.name}")
 
+            params_pyscf = self.params_pyscf.copy()
+            params_slurm = self.params_slurm.copy()
+
             params_pyscf.update(
                 {
                     "charge": mole.charge,
@@ -191,7 +287,7 @@ class SubSet:
                 }
             )
 
-            params_sh.update(
+            params_slurm.update(
                 {
                     "jobname": mole.name,
                     "commands": f"python {mole.name}.py > {mole.name}.log",
@@ -201,11 +297,11 @@ class SubSet:
             with open(os.path.join(new_folder, f"{mole.name}.py"), "w") as f:
                 f.write(input_template.substitute(params_pyscf))
             with open(os.path.join(new_folder, f"{mole.name}.sh"), "w") as f:
-                f.write(sh_template.substitute(params_sh))
+                f.write(sh_template.substitute(params_slurm))
 
         print("Done!")
 
-    def input_gen_gaussian(self, params_gaussian: dict, params_sh: dict):
+    def input_gen_gaussian(self):
         print("Generating input files for Gaussian...")
 
         input_file = pkg.resource_string("qcds", "templates/gaussian.template")
@@ -218,9 +314,19 @@ class SubSet:
             # self.mole_paths.update({mole.name: new_folder})
             print(f"> {mole.name}")
 
-            params_gaussian.update({"chk": f"{mole.name}.chk", "xyz": str(mole)})
+            params_gaussian = self.params_gaussian.copy()
+            params_slurm = self.params_slurm.copy()
 
-            params_sh.update(
+            params_gaussian.update(
+                {
+                    "chk": f"{mole.name}.chk",
+                    "xyz": str(mole),
+                    "titlecard": mole.name,
+                    "extra": "",
+                }
+            )
+
+            params_slurm.update(
                 {
                     "jobname": mole.name,
                     "commands": f"g16 {mole.name}.gjf",
@@ -230,11 +336,11 @@ class SubSet:
             with open(os.path.join(new_folder, f"{mole.name}.gjf"), "w") as f:
                 f.write(input_template.substitute(params_gaussian))
             with open(os.path.join(new_folder, f"{mole.name}.sh"), "w") as f:
-                f.write(sh_template.substitute(params_sh))
+                f.write(sh_template.substitute(params_slurm))
 
         print("Done!")
 
-    def input_gen_psi4(self, params_psi4: dict, params_sh: dict):
+    def input_gen_psi4(self):
         print("Generating input files for Psi4...")
 
         input_file = pkg.resource_string("qcds", "templates/psi4.template")
@@ -247,6 +353,9 @@ class SubSet:
             # self.mole_paths.update({mole.name: new_folder})
             print(f"> {mole.name}")
 
+            params_psi4 = self.params_psi4.copy()
+            params_slurm = self.params_slurm.copy()
+
             params_psi4.update(
                 {
                     "xyz": str(mole),
@@ -254,7 +363,7 @@ class SubSet:
                 }
             )
 
-            params_sh.update(
+            params_slurm.update(
                 {
                     "jobname": mole.name,
                     "commands": f"python {mole.name}.py > {mole.name}.log",
@@ -264,13 +373,11 @@ class SubSet:
             with open(os.path.join(new_folder, f"{mole.name}.py"), "w") as f:
                 f.write(input_template.substitute(params_psi4))
             with open(os.path.join(new_folder, f"{mole.name}.sh"), "w") as f:
-                f.write(sh_template.substitute(params_sh))
+                f.write(sh_template.substitute(params_slurm))
 
         print("Done")
 
-    def input_gen(
-        self, software: str, path: str, prefix: str, suffix: str, params: dict
-    ):
+    def input_gen(self, software: str, path: str, prefix: str, suffix: str):
         self.mole_configs = self.read_moleconfigs(self.geom_path, self.list_file)
 
         self.input_path = os.path.join(
@@ -281,34 +388,67 @@ class SubSet:
 
         match software.lower():
             case "abacus":
-                self.input_gen_abacus(
-                    params["abacus_input"],
-                    params["abacus_stru"],
-                    params["abacus_kpt"],
-                    params["sh"],
-                )
+                self.input_gen_abacus()
             case "pyscf":
-                self.input_gen_pyscf(params["pyscf"], params["sh"])
+                self.input_gen_pyscf()
             case "gaussian":
-                self.input_gen_gaussian(params["gaussian"], params["sh"])
+                self.input_gen_gaussian()
             case "psi4":
-                self.input_gen_psi4(params["psi4"], params["sh"])
+                self.input_gen_psi4()
             case _:
-                raise ValueError("Software not supported")
+                raise ValueError("Software not supported yet")
 
     def output_read_abacus(self):
-        pass
+        for mole in self.mole_configs:
+            out_folder = os.path.join(self.out_path, mole.name)
+            with open(
+                os.path.join(
+                    out_folder,
+                    f"OUT.abacus/running_{self.params_abacus_input["calculation"]}.log",
+                ),
+                "r",
+            ) as f:
+                lines = f.readlines()
+            for line in lines:
+                if line.startswith(" !FINAL_ETOT_IS "):
+                    eng = float(line.split()[1])
+                    self.mole_eng.update({mole.name: EngUnit(eng, unit="eV")})
 
     def output_read_pyscf(self):
-        pass
+        for mole in self.mole_configs:
+            out_folder = self.out_path
+            with open(os.path.join(out_folder, f"{mole.name}.out"), "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                if line.startswith("converged SCF energy = "):
+                    eng = float(line.split()[4])
+                    self.mole_eng.update({mole.name: EngUnit(eng, unit="Hartree")})
 
     def output_read_gaussian(self):
-        pass
+        for mole in self.mole_configs:
+            out_folder = self.out_path
+            with open(os.path.join(out_folder, f"{mole.name}.out"), "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                if line.startswith(" SCF Done:"):
+                    eng = float(line.split()[4])
+                    self.mole_eng.update({mole.name: EngUnit(eng, unit="Hartree")})
 
     def output_read_psi4(self):
-        pass
+        for mole in self.mole_configs:
+            out_folder = self.out_path
+            with open(os.path.join(out_folder, f"{mole.name}.out"), "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                if line.startswith("    Total Energy = "):
+                    eng = float(line.split()[3])
+                    self.mole_eng.update({mole.name: EngUnit(eng, unit="Hartree")})
 
-    def output_read(self, software: str):
+    def output_read(self, software: str, path: str, prefix: str, suffix: str):
+        self.out_path = os.path.join(path, f"{prefix}_{software}_{self.name}_{suffix}")
+        if not os.path.exists(self.out_path):
+            raise FileNotFoundError(f"Output folder {self.out_path} not found")
+
         match software.lower():
             case "abacus":
                 self.output_read_abacus()
